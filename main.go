fe9ae35b3929d44db747e58f0dfdba770cd6190b
Marc Leef <leefmarc@gmail.com>
2016-04-03 23:27:15 -0400

    Fixed spacing for real

--- main.go
+++ main.go
@@ -4,13 +4,13 @@ import (
   "fmt"
 )
 func main() {
-	ind := docindexing.NewIndex()
-	ind.AddTerm("marc")
-	ind.AddTerm("john")
-	ind.AddTerm("aaron")
-	
-	terms := docindexing.ReadFile("/Users/marcleef/Desktop/sample.txt")
-	for key, val := range terms {
-		fmt.Printf("%s : %d\n", key, val.Frequency)
-	}
+  ind := docindexing.NewIndex()
+  ind.AddTerm("marc")
+  ind.AddTerm("john")
+  ind.AddTerm("aaron")
+  
+  terms := docindexing.ReadFile("/Users/marcleef/Desktop/sample.txt")
+  for key, val := range terms {
+    fmt.Printf("%s : %d\n", key, val.Frequency)
+  }
 }
