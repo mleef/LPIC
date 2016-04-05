850e5a3712b615ba5da243d97200d36a5761a38d
Marc Leef <leefmarc@gmail.com>
2016-04-05 16:09:35 -0400

    Updated formatting using gofmt

--- docindexing/index.go
+++ docindexing/index.go
@@ -1,64 +1,63 @@
 package docindexing
 
 import (
-  "container/list"
-  "sync"
-  "runtime"
-  "log"
+	"container/list"
+	"log"
+	"runtime"
+	"sync"
 )
 
 // Highest level inverted index structure
 type InvertedIndex struct {
-  Terms map[string]*TermEntry
-  TermCount int
-  IndexLock *sync.Mutex
-  
+	Terms     map[string]*TermEntry
+	TermCount int
+	IndexLock *sync.Mutex
 }
 
 // Stores terms and their respective document entries
-type TermEntry struct { 
-  Term string
-  Frequency int
-  Documents *list.List
-  EntryLock *sync.Mutex
+type TermEntry struct {
+	Term      string
+	Frequency int
+	Documents *list.List
+	EntryLock *sync.Mutex
 }
 
 // Stores document metadata as well as additional term information
 type DocumentEntry struct {
-  Name string
-  Path string
-  Size int64
-  Frequency int
-  Positions *list.List
+	Name      string
+	Path      string
+	Size      int64
+	Frequency int
+	Positions *list.List
 }
 
 // Constructs new inverted index
 func NewIndex() *InvertedIndex {
-  return &InvertedIndex{Terms: make(map[string]*TermEntry), TermCount: 0, IndexLock: &sync.Mutex{}}
+	return &InvertedIndex{Terms: make(map[string]*TermEntry), TermCount: 0, IndexLock: &sync.Mutex{}}
 }
 
 // Adds new term entry to the index
 func (ind *InvertedIndex) AddTerm(term string) {
-  ind.IndexLock.Lock()
-  // Make sure we aren't overwriting existing term
-  if _, found := ind.Terms[term]; !found {
-    log.Printf("Adding new term %s to index", term)
-    ind.Terms[term] = &TermEntry{Term: term, Frequency: 0, Documents: list.New(), EntryLock: &sync.Mutex{}}
-    ind.TermCount++;
-  }
-  ind.IndexLock.Unlock()
-  runtime.Gosched()
+	ind.IndexLock.Lock()
+	// Make sure we aren't overwriting existing term
+	if _, found := ind.Terms[term]; !found {
+		log.Printf("Adding new term %s to index", term)
+		ind.Terms[term] = &TermEntry{Term: term, Frequency: 0, Documents: list.New(), EntryLock: &sync.Mutex{}}
+		ind.TermCount++
+	}
+	ind.IndexLock.Unlock()
+	runtime.Gosched()
 }
 
 // Adds new document entry to given term entry
 func (ind *InvertedIndex) AddDocument(term string, document *DocumentEntry) {
-  // Make sure term is in index
-  ind.AddTerm(term)
-  log.Printf("Adding new document to term %s's document list", term)
-  // Safely add document to term list
-  ind.Terms[term].EntryLock.Lock()
-  ind.Terms[term].Frequency += document.Frequency
-  ind.Terms[term].Documents.PushBack(document)
-  ind.Terms[term].EntryLock.Unlock()
-  runtime.Gosched()
+	// Make sure term is in index
+	ind.AddTerm(term)
+	log.Printf("Adding new document to term %s's document list", term)
+	// Safely add document to term list
+	ind.Terms[term].EntryLock.Lock()
+	ind.Terms[term].Frequency += document.Frequency
+	ind.Terms[term].Documents.PushBack(document)
+	ind.Terms[term].EntryLock.Unlock()
+	runtime.Gosched()
 }
--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
+
 import (
-  "github.com/mleef/lpic/docindexing"
-  "fmt"
-  "os"
+	"fmt"
+	"github.com/mleef/lpic/docindexing"
+	"os"
 )
+
 func main() {
-  ind := docindexing.NewIndex()
-  ind.AddTerm("marc")
-  ind.AddTerm("john")
-  ind.AddTerm("aaron")
-  
-  terms, err := docindexing.ReadFile(os.Args[1])
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    for key, val := range terms {
-      fmt.Printf("%s : %d\n", key, val.Frequency)
-    }
-  }
+	ind := docindexing.NewIndex()
+	ind.AddTerm("marc")
+	ind.AddTerm("john")
+	ind.AddTerm("aaron")
+
+	terms, err := docindexing.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		for key, val := range terms {
+			fmt.Printf("%s : %d\n", key, val.Frequency)
+		}
+	}
 }
